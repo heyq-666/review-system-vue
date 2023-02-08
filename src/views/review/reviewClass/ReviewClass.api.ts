39d108761fccf351f1be71cccc87dd1b6378423a
heyq <heyq0918@163.com>
2023-02-08 17:44:37 +0800

    first commit

---
+++ src/views/review/reviewClass/ReviewClass.api.ts
@@ -0,0 +1,137 @@
+import {defHttp} from '/@/utils/http/axios';
+import { useMessage } from "/@/hooks/web/useMessage";
+
+const { createConfirm } = useMessage();
+
+enum Api {
+  list = '/reviewClass/reviewClass/list',
+  save='/reviewClass/reviewClass/add',
+  edit='/reviewClass/reviewClass/edit',
+  deleteOne = '/reviewClass/reviewClass/delete',
+  deleteBatch = '/reviewClass/reviewClass/deleteBatch',
+  importExcel = '/reviewClass/reviewClass/importExcel',
+  exportXls = '/reviewClass/reviewClass/exportXls',
+  reviewQuestionList = '/reviewClass/reviewClass/listReviewQuestionByMainId',
+  reviewQuestionSave='/reviewClass/reviewClass/addReviewQuestion',
+  reviewQuestionEdit='/reviewClass/reviewClass/editReviewQuestion',
+  reviewQuestionDelete = '/reviewClass/reviewClass/deleteReviewQuestion',
+  reviewQuestionDeleteBatch = '/reviewClass/reviewClass/deleteBatchReviewQuestion',
+  publishBatch = '/reviewClass/reviewClass/publishBatch'
+}
+/**
+ * 导出api
+ * @param params
+ */
+export const getExportUrl = Api.exportXls;
+
+/**
+ * 导入api
+ */
+export const getImportUrl = Api.importExcel;
+/**
+ * 列表接口
+ * @param params
+ */
+export const list = (params) =>
+  defHttp.get({url: Api.list, params});
+
+/**
+ * 删除单个
+ */
+export const deleteOne = (params,handleSuccess) => {
+  return defHttp.delete({url: Api.deleteOne, params}, {joinParamsToUrl: true}).then(() => {
+    handleSuccess();
+  });
+}
+/**
+ * 批量删除
+ * @param params
+ */
+export const batchDelete = (params, handleSuccess) => {
+  createConfirm({
+    iconType: 'warning',
+    title: '确认删除',
+    content: '是否删除选中数据',
+    okText: '确认',
+    cancelText: '取消',
+    onOk: () => {
+      return defHttp.delete({url: Api.deleteBatch, data: params}, {joinParamsToUrl: true}).then(() => {
+        handleSuccess();
+      });
+    }
+  });
+}
+/**
+ * 保存或者更新
+ * @param params
+ */
+export const saveOrUpdate = (params, isUpdate) => {
+  let url = isUpdate ? Api.edit : Api.save;
+  return defHttp.post({url: url, params});
+}
+/**
+ * 列表接口
+ * @param params
+ */
+export const reviewQuestionList = (params) => {
+  if(params['classId']){
+    return defHttp.get({url: Api.reviewQuestionList, params});
+  }
+  return Promise.resolve({});
+}
+
+
+/**
+ * 删除单个
+ */
+export const reviewQuestionDelete = (params,handleSuccess) => {
+  return defHttp.delete({url: Api.reviewQuestionDelete, params}, {joinParamsToUrl: true}).then(() => {
+    handleSuccess();
+  });
+}
+/**
+ * 批量删除
+ * @param params
+ */
+export const reviewQuestionDeleteBatch = (params, handleSuccess) => {
+  createConfirm({
+    iconType: 'warning',
+    title: '确认删除',
+    content: '是否删除选中数据',
+    okText: '确认',
+    cancelText: '取消',
+    onOk: () => {
+      return defHttp.delete({url: Api.reviewQuestionDeleteBatch, data: params}, {joinParamsToUrl: true}).then(() => {
+        handleSuccess();
+      });
+    }
+  });
+}
+/**
+ * 保存或者更新
+ * @param params
+ */
+export const  reviewQuestionSaveOrUpdate = (params, isUpdate) => {
+  let url = isUpdate ? Api.reviewQuestionEdit : Api.reviewQuestionSave;
+  return defHttp.post({url: url, params});
+}
+/**
+ * 导入
+ */
+export const reviewQuestionImportUrl = '/reviewClass/reviewClass/importReviewQuestion'
+
+/**
+ * 导出
+ */
+export const reviewQuestionExportXlsUrl = '/reviewClass/reviewClass/exportReviewQuestion'
+
+/**
+ * 发布/停用量表
+ * @param params
+ * @param handleSuccess
+ */
+export const publishBatch = (params, handleSuccess) => {
+  return defHttp.put({ url: Api.publishBatch, params }).then(() => {
+    handleSuccess();
+  });
+};
---
+++ src/views/review/reviewClass/ReviewClass.data.ts
@@ -0,0 +1,178 @@
+import {BasicColumn} from '/@/components/Table';
+import {FormSchema} from '/@/components/Table';
+import { rules} from '/@/utils/helper/validator';
+import { render } from '/@/utils/common/renderUtils';
+//列表数据
+export const columns: BasicColumn[] = [
+   {
+    title: '排序ID',
+    align:"center",
+    dataIndex: 'sortId'
+   },
+   {
+    title: '量表名称',
+    align:"center",
+    dataIndex: 'title'
+   },
+   {
+    title: '状态',
+    align:"center",
+    dataIndex: 'status_dictText'
+   },
+];
+//查询数据
+export const searchFormSchema: FormSchema[] = [
+	{
+      label: "量表名称",
+      field: "title",
+      component: 'Input',
+      colProps: {span: 6},
+ 	},
+     {
+      label: "创建时间",
+      field: "createTime",
+      component: 'RangePicker',
+      componentProps: {
+          valueType: 'Date',
+          showTime:true
+      },
+      colProps: {span: 6},
+	},
+];
+
+//表单数据
+export const formSchema: FormSchema[] = [
+  {
+    label: '排序ID',
+    field: 'sortId',
+    component: 'InputNumber',
+  },
+  {
+    label: '量表名称',
+    field: 'title',
+    component: 'Input',
+  },
+  {
+    label: '量表简介',
+    field: 'classDesc',
+    component: 'InputTextArea',//TODO 注意string转换问题
+  },
+  {
+    label: '引导语',
+    field: 'guide',
+    component: 'InputTextArea',//TODO 注意string转换问题
+  },
+  {
+    label: '是否计费',
+    field: 'charge',
+    component: 'RadioGroup',
+    defaultValue: 0,
+    componentProps: ({ formModel }) => {
+      return {
+        options: [
+          { label: '是', value: 1, key: '1' },
+          { label: '否', value: 0, key: '0' },
+        ],
+        onChange: () => {
+          formModel.charge = 0 && (formModel.orgPrice = []) && (formModel.dicountPrice = []);
+        },
+      };
+    },
+  },
+  {
+    label: '量表价格',
+    field: 'orgPrice',
+    component: 'InputNumber',
+    ifShow: ({ values }) => values.charge == 1,
+  },
+  {
+    label: '优惠金额',
+    field: 'dicountPrice',
+    component: 'InputNumber',
+    ifShow: ({ values }) => values.charge == 1,
+  },
+  {
+    label: '封面图片',
+    field: 'bannerImg',
+     component: 'JImageUpload',
+     componentProps:{
+        fileMax:1
+     },
+  },
+  {
+    label: '是否视频测评',
+    field: 'videoAnalysis',
+    component: 'RadioGroup',
+    defaultValue: 0,
+    componentProps: {
+       options: [
+          { label: '是', value: 1, key: '1' },
+          { label: '否', value: 0, key: '0' },
+       ],
+    },
+  },
+  {
+    label: '是否热门',
+    field: 'type',
+    component: 'JDictSelectTag',
+    componentProps: {
+      options: [
+        { label: '热门', value: 1, key: '1' },
+        { label: '普通', value: 0, key: '0' },
+      ],
+    },
+  },
+	// TODO 主键隐藏字段，目前写死为ID
+	{
+	  label: '',
+	  field: 'id',
+	  component: 'Input',
+	  show: false
+	},
+];
+
+//子表列表数据
+export const reviewQuestionColumns: BasicColumn[] = [
+  {
+    title: '题目序号',
+    align:"center",
+    dataIndex: 'questionNum',
+  },
+  {
+    title: '题目内容',
+    align:"center",
+    dataIndex: 'content'
+  }
+];
+//子表表单数据
+export const reviewQuestionFormSchema: FormSchema[] = [
+  // TODO 子表隐藏字段，目前写死为ID
+  {
+    label: '',
+    field: 'id',
+    component: 'Input',
+    show: false
+  },
+  {
+    label: '题目类型',
+    field: 'questionType',
+    component: 'JDictSelectTag',
+    componentProps: {
+      options: [
+        { label: '单选题', value: 1, key: '1' },
+        { label: '问答题', value: 2, key: '2' },
+        { label: '多选题', value: 3, key: '3' },
+      ],
+    },
+  },
+  {
+    label: '题目内容',
+    field: 'content',
+    component: 'InputTextArea',
+  },
+  {
+    label: '选项',
+    field: 'answer',
+    component:'Input'
+  }
+];
